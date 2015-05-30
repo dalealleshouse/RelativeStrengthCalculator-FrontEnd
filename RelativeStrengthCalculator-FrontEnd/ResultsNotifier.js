a38efb0ad69764b74b173a44e54b0367e8daff33
Dale Alleshouse <dale.alleshouse@burke.com>
2015-05-30 12:02:51 -0400

    notifier

---
+++ RelativeStrengthCalculator-FrontEnd/ResultsNotifier.js
@@ -0,0 +1,14 @@
+﻿var ResultsNotifier = ResultsNotifier || {};
+(function(resultsNotifier, calculator) {
+    ResultsNotifier.Notifier = function(callback) {
+        var results = [];
+
+        this.addScore = function(params) {
+            calculator.score(params, function(response) {
+                results.push({ request: params, result: response });
+                callback(results);
+            });
+        }
+    }
+
+})(ResultsNotifier, Calculator);
