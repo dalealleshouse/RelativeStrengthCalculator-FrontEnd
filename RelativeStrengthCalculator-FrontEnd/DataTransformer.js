a240e4300abaae3a7ccd7eb483e7e0c3af08d4bf
Dale Alleshouse <dale.alleshouse@burke.com>
2015-05-30 13:58:07 -0400

    clear old circles

--- RelativeStrengthCalculator-FrontEnd/DataTransformer.js
+++ RelativeStrengthCalculator-FrontEnd/DataTransformer.js
@@ -6,6 +6,7 @@
 
         var counter = 1;
         var previous = 0;
+        data = getRelativePlacement(data);
         data.forEach(function (o) {
             var adjustedScore = o.result.Score / 10;
             previous = previous + adjustedScore;
@@ -14,7 +15,7 @@
                 "x_axis": counter * 10 + previous,
                 "y_axis": counter * 10 + previous,
                 "radius": adjustedScore,
-                "color": "green"
+                "color": o.color
             });
 
             counter++;
@@ -22,4 +23,33 @@
 
         return circleData;
     };
+
+    function getColor(num) {
+        if (num <= 1)
+            return "green";
+        if (num <= 2)
+            return "yellow";
+
+        return "red";
+    }
+
+    function getRelativePlacement(scores) {
+        scores.sort(function(a, b) {
+            if (a.result.Score > b.result.Score) {
+                return -1;
+            }
+            if (a.result.Score < b.result.Score) {
+                return 1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+
+        var totalNumber = scores.length;
+        for (var i = 0; i < scores.length; i++) {
+            scores[i].color = getColor(i);
+        }
+
+        return scores;
+    }
 })(DataTransformer);
