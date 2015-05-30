41786af8ff835b5adc855382fe658dc8e45aa781
Dale Alleshouse <dale.alleshouse@burke.com>
2015-05-30 13:33:56 -0400

    Fix sizing

--- RelativeStrengthCalculator-FrontEnd/DataTransformer.js
+++ RelativeStrengthCalculator-FrontEnd/DataTransformer.js
@@ -5,11 +5,15 @@
         var circleData = [];
 
         var counter = 1;
+        var previous = 0;
         data.forEach(function (o) {
+            var adjustedScore = o.result.Score / 10;
+            previous = previous + adjustedScore;
+
             circleData.push({
-                "x_axis": counter * 10,
-                "y_axis": counter * 10,
-                "radius": o.result.Score,
+                "x_axis": counter * 10 + previous,
+                "y_axis": counter * 10 + previous,
+                "radius": adjustedScore,
                 "color": "green"
             });
 
