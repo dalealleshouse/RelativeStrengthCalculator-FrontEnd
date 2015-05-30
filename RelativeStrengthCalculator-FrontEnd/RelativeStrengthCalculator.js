88305daacf33692022405d98384920cc41b52a17
Dale Alleshouse <dale.alleshouse@burke.com>
2015-05-30 11:27:11 -0400

    added relative strength calc api object

---
+++ RelativeStrengthCalculator-FrontEnd/RelativeStrengthCalculator.js
@@ -0,0 +1,35 @@
+﻿var Calculator = Calculator || {};
+(function(calculator) {
+
+    calculator.score = function(params, callback) {
+        var url = buildUrl(params);
+
+        $.get(url, function(data) {
+            callback(data);
+        });
+    };
+
+    calculator.coefficent = function(params, callback) {
+        delete params.weight;
+        var url = buildUrl(params);
+
+        $.get(url, function(data) {
+            callback(data);
+        });
+    };
+
+    function buildUrl(requestParams) {
+        var url = "http://relativestrengthcalculatorapi.azurewebsites.net/relativeStrength/"
+            + requestParams.algorithm
+            + "/" + requestParams.weightUnit
+            + "/" + requestParams.sex
+            + "/" + requestParams.bodyWeight
+            + "/";
+
+        if (requestParams.weight) {
+            url = url + requestParams.weight + "/";
+        }
+
+        return url;
+    }
+})(Calculator);
