60abd9702a677cc807fbe8639fbfcaac20f64ad7
Dale Alleshouse <dale.alleshouse@burke.com>
2015-05-30 13:14:54 -0400

    Data transformer

---
+++ RelativeStrengthCalculator-FrontEnd/DataTransformer.js
@@ -0,0 +1,21 @@
+﻿var DataTransformer = DataTransformer || {};
+
+(function (dt) {
+    dt.transform = function(data) {
+        var circleData = [];
+
+        var counter = 1;
+        data.forEach(function (o) {
+            circleData.push({
+                "x_axis": counter * 10,
+                "y_axis": counter * 10,
+                "radius": o.result.Score,
+                "color": "green"
+            });
+
+            counter++;
+        });
+
+        return circleData;
+    };
+})(DataTransformer);
